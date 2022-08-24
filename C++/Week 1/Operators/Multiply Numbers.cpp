0f815892832abff7af4d193679c2a0f5ed32e74c
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-24 22:43:40 +0530

    Create Multiply Numbers.cpp

---
+++ C++/Week 1/Operators/Multiply Numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int x,y;
+    cin>>x>>y;
+    int product=x*y;
+    cout<<product;
+    
+    return 0;
+  }
