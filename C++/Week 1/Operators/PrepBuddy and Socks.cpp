6662b2f9c8980b5435a11d80b55bc6bb9a050faa
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-24 22:46:28 +0530

    Create PrepBuddy and Socks.cpp

---
+++ C++/Week 1/Operators/PrepBuddy and Socks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int t;
+    cin>>t;
+    while(t!=0){
+      int n;
+      cin>>n;
+      cout<<n+1<<endl;
+        t=t-1;
+      
+    }
+    return 0;
+  }
