3569dea553f215ac4f10167088c79b8f0aafe1e6
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-24 22:45:36 +0530

    Create Last Digit of Number.cpp

---
+++ C++/Week 1/Operators/Last Digit of Number.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int x;
+    cin>>x;
+    int last_digit=x%10;
+    cout<<last_digit;
+    
+    return 0;
+  }
