19dca2fce4115b9134b31d1eb631a4198d31fef8
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-24 22:45:00 +0530

    Create Modulus Ten.cpp

---
+++ C++/Week 1/Operators/Modulus Ten.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int x;
+    cin>>x;
+    int rem=x%10;
+    cout<<rem;
+    
+    return 0;
+  }
