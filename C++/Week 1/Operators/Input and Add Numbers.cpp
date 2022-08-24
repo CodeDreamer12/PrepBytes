b580af22a7bfce1cd8cac14935e3b10b5d1afc7c
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-24 22:42:59 +0530

    Create Input and Add Numbers.cpp

---
+++ C++/Week 1/Operators/Input and Add Numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int x,y;
+    cin>>x>>y;
+    int sum=x+y;
+    cout<<sum;
+    
+    return 0;
+  }
