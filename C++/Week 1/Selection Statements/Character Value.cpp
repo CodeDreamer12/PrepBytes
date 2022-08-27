1c211f88cf95c2ab188995de16a071f7530997d6
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-27 22:52:16 +0530

    Create Character Value.cpp

---
+++ C++/Week 1/Selection Statements/Character Value.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    char ch;
+    cin>>ch;
+    if(ch=='P' || ch=='p'){
+      cout<<"PrepBytes"<<endl;
+    }
+    else if(ch=='Z' || ch=='z'){
+      cout<<"Zenith"<<endl;
+    }
+    else if(ch=='E' || ch=='e'){
+      cout<<"Expert Coder"<<endl;
+    }
+    else{
+      cout<<"Data Structure"<<endl;
+    }
+    
+    return 0;
+  }
